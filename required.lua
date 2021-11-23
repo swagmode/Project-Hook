b8ad8dc76a2e194b8e2d781fc55b87ed000b47d4
Adam <52005747+SpiritXmas@users.noreply.github.com>
2021-11-23 23:04:52 +0000

    Update required.lua

--- required.lua
+++ required.lua
@@ -1,14 +1,10 @@
 getgenv().modifiedWST = false
 getgenv().modifiedWS = 16
-getgenv().modifiedJPT = false
-getgenv().modifiedJP = 50
 
 do
     local old; old = hookmetamethod(game, "__index", function(a,b)
         if tostring(a) == "Humanoid" and tostring(b) == "WalkSpeed" and getgenv().modifiedWST then
             return 16
-        elseif tostring(a) == "Humanoid" and tostring(b) == "JumpPower" and getgenv().modifiedJPT then
-            return 50
         end
             
         return old(a,b)
@@ -17,17 +13,10 @@ do
     local old; old = hookmetamethod(game, "__newindex", function(a,b,c)
         if a.ClassName == "Humanoid" and tostring(b) == "WalkSpeed" and getgenv().modifiedWST then
             return old(a,b,getgenv().modifiedWS)
-        elseif a.ClassName == "Humanoid" and tostring(b) == "JumpPower" and getgenv().modifiedJPT then
-            return old(a,b,getgenv().modifiedJP)
         end
         return old(a,b,c)
     end)
 end
-
-game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(char)
-    local humanoid = char:WaitForChild("Humanoid")
-    humanoid.JumpPower = 50
-end)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpiritXmas/Project-Hook/main/main.lua"))()
 
